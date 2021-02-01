90afa267154da24f2fde3e2c0b075f8268aebbdf
shanetomlinson <tiedupinside@gmail.com>
2021-01-31 23:47:33 -0700

    cooded UI self interaction and the appearing of the food. downloaded some food based assets to go through

---
+++ Assets/Scripts/WelcomeButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WelcomeButton : MonoBehaviour
+{
+    public GameObject userMenu;
+    public GameObject welcomeMenu;
+    public GameObject adultMenu;
+    public GameObject KidsMenu;
+
+    public void welcomeScreen()
+    {
+        welcomeMenu.SetActive(false);
+        userMenu.SetActive(true);
+    }
+
+    public void grownUpMenu()
+    {
+        userMenu.SetActive(false);
+        adultMenu.SetActive(true);
+
+    }
+
+    public void kidsMenu()
+    {
+        userMenu.SetActive(false);
+        KidsMenu.SetActive(true);
+
+    }
+}
---
+++ Assets/Scripts/foodButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class foodButton : MonoBehaviour
+{
+
+    public GameObject plateOfFood;
+
+    public void makeFoodAppear()
+    {
+        plateOfFood.SetActive(true);
+    }
+}
